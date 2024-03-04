0431399c84fe1c306049ba3dd91e02ffc4f75da2
NuiN99 <endyverse@gmail.com>
2024-03-04 12:14:39 -0800

    Swinging visuals

--- ROCK VR/Assets/Features/Player/Movement/CharacterController/Scripts/Swinging/PlayerSwingingXR.cs
+++ ROCK VR/Assets/Features/Player/Movement/CharacterController/Scripts/Swinging/PlayerSwingingXR.cs
@@ -1,32 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using NuiN.NExtensions;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerSwinging : MonoBehaviour
+public class PlayerSwingingXR : MonoBehaviour
 {
     Transform _connectionPoint;
     Rigidbody _anchor;
     bool _addedRigidbody;
     bool _attached;
 
-    [SerializeField] Transform visualRoot;
+    [SerializeField] Transform root;
     
     [SerializeField] float maxAttachDistance = 25f;
     
-    [SerializeField] KeyCode activateKey = KeyCode.Mouse1;
-    [SerializeField] Transform head;
+    [SerializeField] InputActionReference activateAction;
     [SerializeField] LayerMask attachableLayers;
 
-    [SerializeField] Rigidbody rb;
     [SerializeField] ConfigurableJoint joint;
 
+    [SerializeField] LineRenderer indicatorLineRenderer;
     [SerializeField] LineRenderer ropeLineRenderer;
-
+    
     Vector3 ConnectedAnchorWorld => _anchor.transform.TransformPoint(joint.connectedAnchor);
 
     void Awake()
     {
+        indicatorLineRenderer.positionCount = 2;
         ropeLineRenderer.positionCount = 2;
         ropeLineRenderer.enabled = false;
         _connectionPoint = new GameObject("RopeConnectionPoint").transform;
@@ -34,23 +32,26 @@ public class PlayerSwinging : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(activateKey)) Activate();
-        else if(Input.GetKeyUp(activateKey)) Detach();
+        if (activateAction.action.WasPressedThisFrame()) Activate();
+        else if(activateAction.action.WasReleasedThisFrame()) Detach();
 
         if (!_attached)
         {
             joint.connectedAnchor = transform.position;
+            
+            indicatorLineRenderer.SetPosition(0, root.position);
+            indicatorLineRenderer.SetPosition(1, root.position + root.forward * maxAttachDistance);
         }
         else
         {
-            ropeLineRenderer.SetPosition(0, visualRoot.position);
+            ropeLineRenderer.SetPosition(0, root.position);
             ropeLineRenderer.SetPosition(1, _connectionPoint.position);
         }
     }
 
     void Activate()
     {
-        if (!Physics.Raycast(head.position, head.forward, out RaycastHit hit, maxAttachDistance, attachableLayers)) return;
+        if (!Physics.Raycast(root.position, root.forward, out RaycastHit hit, maxAttachDistance, attachableLayers)) return;
         if (!hit.collider.TryGetComponent(out _anchor))
         {
             _anchor = hit.collider.gameObject.AddComponent<Rigidbody>();
@@ -73,6 +74,7 @@ public class PlayerSwinging : MonoBehaviour
         _connectionPoint.SetParent(hit.collider.transform);
         
         ropeLineRenderer.enabled = true;
+        indicatorLineRenderer.enabled = false;
         
         _attached = true;
     }
@@ -83,6 +85,8 @@ public class PlayerSwinging : MonoBehaviour
         if(_anchor != null && _addedRigidbody) Destroy(_anchor);
 
         ropeLineRenderer.enabled = false;
+        indicatorLineRenderer.enabled = true;
+
         _attached = false;
     }
 
@@ -90,7 +94,7 @@ public class PlayerSwinging : MonoBehaviour
     {
         if (!_attached)
         {
-            if (!Physics.Raycast(head.position, head.forward, out RaycastHit hit, maxAttachDistance, attachableLayers)) return;
+            if (!Physics.Raycast(root.position, root.forward, out RaycastHit hit, maxAttachDistance, attachableLayers)) return;
             Gizmos.DrawSphere(hit.point, 0.25f);
             return;
         }
